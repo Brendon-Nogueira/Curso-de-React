1f23109fbd831106dc27c8313ef64bde25b66f38
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-12-22 16:34:47 -0300

    first commit

---
+++ fundamentos02/fundamentos/src/App.js
@@ -0,0 +1,17 @@
+// components
+import FirstComponent  from './components/FirstComponent';
+
+// import do Style
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo os fundamentos do REACT</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fundamentos02/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+const FirstComponent = () =>{
+    
+
+    
+    return (
+        <div>
+            <h1 className="txt"> My first component </h1>
+        </div>
+    );
+};
+
+export default FirstComponent;
