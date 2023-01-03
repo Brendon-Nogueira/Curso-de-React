59579f74454445b3547aebcfb351138e24309b15
Brendon-Nogueira <brendonnogueira21@gmail.com>
2023-01-03 19:46:56 -0300

    terminando módulo de css

--- css_react04/css/src/App.js
+++ css_react04/css/src/App.js
@@ -1,9 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+//components
+import MyComponent from './components/MyComponent';
+import Title from './components/Title'
 
 function App() {
+
+  const x = 15
+  const [name] = useState('Brendon')
+
+
+ // const redTitle =   true
+    const redTitle =   false
+
   return (
     <div className="App">
+      {/*  CSS GLOBAL  /*/}
+     <h1>React com CSS</h1>
+      {/*  CSS COMPONENT /*/}
+      <MyComponent/>
+     <p>Este conteúdo é do App.js</p>
+      {/*  CSS INLINE /*/}
+      <p style={{color: 'purple', padding: '25px', borderTop: '2px solid purple'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Asperiores neque iusto molestias dolor? 
+        Assumenda, ad rem quisquam necessitatibus qui, 
+        possimus aliquam eveniet quia labore sequi illo et libero 
+        odio ut.
+      </p>
+
+       {/*  CSS INLINE DINÂMICO/*/}
+      <p style={x < 10 ? ({color: 'red'}) : ({color: 'blue'}) }> CSS INLINE DINÂMICO</p>
+      <p style={x > 10 ? ({color: 'red'}) : ({color: 'blue'}) }> CSS INLINE DINÂMICO</p>
+      <p style={name === 'Brendon' ? {color: 'green'} : null}> CSS INLINE DINÂMICO</p>
+
+       {/*  CLASSES CSS DINAMICO/*/}
+      <h2 className={ redTitle ? 'red-title' : 'title'}>Este título vai ter classe dinâmica</h2>
+
+       {/* CSS MODULES/*/}
+      <Title/>
+
+
+
      
     </div>
   );
---
+++ css_react04/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+ const MyComponent = () => {
+   return (
+     <div>
+        <h1>CSS do meu component</h1>
+        <p>Conteúdo do component</p>
+        <p className="my-comp-paragraph">Este conteúdo tbm é do componente</p>
+     </div>
+   )
+ }
+ 
+ export default MyComponent
